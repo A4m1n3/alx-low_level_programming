395afec16654f9f720a7b227fee84e9583579389
A4m1n3 <medelhabnouny@gmail.com>
2023-06-17 01:43:23 +0000

    m

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char letter;
+	
+	for (letter = 'z'; letter >= 'a'; letter--)
+	{
+	putchar(letter);
+	}
+
+	putchar('\n');
+	return (0);
+}
