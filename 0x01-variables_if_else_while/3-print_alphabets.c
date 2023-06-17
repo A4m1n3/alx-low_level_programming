d0aacece1dabd5b750863f9d06869357431b1f89
A4m1n3 <medelhabnouny@gmail.com>
2023-06-17 01:13:32 +0000

    m

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	puts("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\n");
+	return (0);
+}
