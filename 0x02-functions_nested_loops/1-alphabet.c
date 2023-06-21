39610a33513b29e75237a095843fc8ca6430c5ba
A4m1n3 <medelhabnouny@gmail.com>
2023-06-21 00:34:50 +0000

    m

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/*
+ * This 
+ * is
+ * a main 
+ * description
+ */
+
+void print_alphabet(void)
+{
+	char letter = 'a';
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
