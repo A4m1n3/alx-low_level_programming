e07a9ba678a047289570cad66976bfb98c8fd277
A4m1n3 <medelhabnouny@gmail.com>
2023-06-21 23:48:54 +0000

    m

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - check the code.
+ *
+ * Return: n look at code again.
+ * @n: is the input we want to check
+ */
+int print_last_digit(int n)
+{
+	return (_abs(n % 10));	
+}
