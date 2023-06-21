450782188cc98e6bf628f64aaeb3ca84bb69dba7
A4m1n3 <medelhabnouny@gmail.com>
2023-06-21 23:08:20 +0000

    m

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,12 +4,11 @@
  * This is a description to 3-islower
  * _islower - check the code
  */
-
-int _islower(int c)
+ int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z') 
+	if (c >= 'a' && c <= 'z')
 		return (1);
-	else 
+	else
 		return (0);
 }
 
