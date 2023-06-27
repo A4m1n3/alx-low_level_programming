98ef9001a598788347e55c071cc88fafad4cd8a3
A4m1n3 <medelhabnouny@gmail.com>
2023-06-27 10:32:05 +0000

    m

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * rev_string -  changes value of pointers
+ * @*s : character
+ * @s : idem
+ * return : void
+ */
+void rev_string(char *s)
+{
+	char *start = s;
+	char *end = s;
+
+	while (*end)
+		end ++;
+	end --;
+
+	for (start < end)
+	{
+		char temp = *start;
+		*start = *end;
+		*end = temp;
+		start++;
+		end--;
+	}
+
+	_putchar('\n');
+}
