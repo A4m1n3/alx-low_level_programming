cb4a89988f541c2f2fefd77d9f3d4649dc12f41b
A4m1n3 <medelhabnouny@gmail.com>
2023-06-27 11:28:31 +0000

    m

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * rev_string -  changes value of pointers
+ * @*s : character
+ * @s : idem
+ * return : void
+ */
+void puts_half(char *str)
+{
+	int start;
+	int i = 0;
+
+	while (*(str + i) != '\0')
+		i++;
+	
+	if (i % 2 == 0)
+		start = i / 2;
+	else 
+		start = (i - 1) / 2;
+
+
+	while ( str + start )
+	{
+		_putchar(str + start);
+		start++;
+	}
+}
