739b89e87d7db3ef8026a0b16bcd325d01a5df8b
A4m1n3 <medelhabnouny@gmail.com>
2023-06-27 09:29:56 +0000

    m

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _strlen -  changes value of pointers
+ * @s : string
+ * return value : voidr
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+	
+	while (*(s+i) != '0')
+		i++;
+	i++;
+	return i;	
+}
