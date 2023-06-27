90c4f5dea94a7cdcf375f44cb0050f0f1fabaeb6
A4m1n3 <medelhabnouny@gmail.com>
2023-06-27 10:19:56 +0000

    m

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,13 +9,13 @@
  */
 void print_rev(char *s)
 {
-	int len=0;
+	int len = 0;
 	int i;
 
-	while(*(s+len) != '\0')
+	while (*(s + len) != '\0')
 		len++;
 
-	for(i = len-1; i >= 0; i--)
+	for (i = len - 1; i >= 0; i--)
 	{
 		_putchar(*(s + i));
 	}
