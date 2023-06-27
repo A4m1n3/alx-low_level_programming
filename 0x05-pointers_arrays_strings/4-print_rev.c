a8ab3844046027ba3563ad4e7b38c4a3c1f4ee52
A4m1n3 <medelhabnouny@gmail.com>
2023-06-27 10:04:39 +0000

    m

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_rev -  changes value of pointers
+ * @*s : character
+ * @s : idem
+ * return : void
+ */
+void print_rev(char *s)
+{
+	int len;
+	int i;
+
+	len = _strlen(s);
+	for(i = len; i >= 0; i--)
+	{
+		_putchar(*(str + i));
+	}
+
+	_putchar('\n');
+}
