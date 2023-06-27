4a5c86ec0ccc4b10aa7008b46c929da3f33588e4
A4m1n3 <medelhabnouny@gmail.com>
2023-06-27 09:15:09 +0000

    m

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * swap_int- changes value of pointers
+ * @a : et
+ * @b : etc
+ * return value : void
+ * @n @*n : variable to pointer
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+	c = *b;
+	*b = *a;
+	
+}
