ee8cd6f612b43cecacc8823e29d493c4eabf04cc
A4m1n3 <medelhabnouny@gmail.com>
2023-08-10 13:38:23 -0700

    m

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* malloc_checked – if ok returns 98
+*
+* @b: allocated memory
+*
+* Return: it returns the pointer to the allocated memory
+*/
+void *malloc_checked(unsigned int b)
+{
+   void *n;
+
+   n = malloc(b);
+   if (n == NULL)
+       exit(98);
+   return (n);
+}
