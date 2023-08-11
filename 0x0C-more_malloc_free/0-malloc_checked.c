95e28f5585a9ae3dc43b278c74f52be655e2f6e4
A4m1n3 <medelhabnouny@gmail.com>
2023-08-11 09:32:30 -0700

    m

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -9,10 +9,9 @@
 */
 void *malloc_checked(unsigned int b)
 {
-   void *n;
-
-   n = malloc(b);
-   if (n == NULL)
-       exit(98);
-return (n);
+	void *n;
+	n = malloc(b);
+	if (n == NULL)
+		exit(98);
+	return (n);
 }
